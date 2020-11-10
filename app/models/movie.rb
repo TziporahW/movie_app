e29dd0069b2cb255270083e41b6e9ef3fe33ee77
Tziporah <tziporawerzberger@gmail.com>
2020-11-09 19:34:12 -0500

    adds active records

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,4 @@
 class Movie < ApplicationRecord
   validates :title, presence: true
-  #validates :plot, presence: true, length: { minimum: 10 }
+  validates :plot, presence: true, length: { minimum: 10 }
 end
