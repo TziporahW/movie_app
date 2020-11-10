bedc99245dd039bac83bbb056f7d73cbec615c9a
Tziporah <tziporawerzberger@gmail.com>
2020-11-09 19:53:09 -0500

    adds belongs_to

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -17,6 +17,7 @@ class Api::ActorsController < ApplicationController
       known_for: params["known_for"],
       age: params["age"],
       gender: params["gender"],
+      movie_id: params["movie_id"],
     )
     @actor.save
     if @actor.save
@@ -34,6 +35,7 @@ class Api::ActorsController < ApplicationController
     @actor.known_for = params["known_for"] || @actor.known_for
     @actor.age = params["age"] || @actor.age
     @actor.gender = params["gender"] || @actor.gender
+    @actor.movie_id = params["movie_id"] || @actor.movie_id
     @actor.save
 
     if @actor.save
