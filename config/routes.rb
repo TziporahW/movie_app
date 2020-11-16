570da5a97a0957dfad9b86d2355de2e18d0908a3
Tziporah <tziporawerzberger@gmail.com>
2020-11-15 23:33:07 -0500

    adds users

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,15 @@ Rails.application.routes.draw do
     post "/movies" => "movies#create"
     patch "/movies/:id" => "movies#update"
     delete "/movies/:id" => "movies#destroy"
+
     get "/actors" => "actors#index"
     get "/actors/:id" => "actors#show"
     post "/actors" => "actors#create"
     patch "/actors/:id" => "actors#update"
     delete "/actors/:id" => "actors#destroy"
+
+    post "/users" => "users#create"
+
+    post "/sessions" => "sessions#create"
   end
 end
